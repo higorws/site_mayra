bfae145c6ea62569d97a6e2b346144c3200cdec3
higorws <higorweberdslv509@yahoo.com>
2017-03-01 14:52:14 -0300

    url amigavel

---
+++ _app/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+define('HOME', 'http://localhost/site_maira');
+define('THEMES', 'mayrahtml');
+define('INCLUDE_PATH',HOME. '/themes/'.THEMES);
+define('REQUIRE_PATH', 'themes/'.THEMES);
+
+$getUrl = strip_tags(trim(filter_input(INPUT_GET, 'url', FILTER_DEFAULT)));
+$setUrl = (empty($setUrl)? 'index' : $getUrl);
+$Url = explode('/', $setUrl);
+
---
+++ themes/mayrahtml/index.php
@@ -0,0 +1,154 @@
+<section title="banner principal" class="banner_principal">
+    <!-- colocar o alt e title da img -->
+    <img class="Foto_banner"title="Sude mental e bem estar" alt="Saúde mental e bem estar" src="<?= INCLUDE_PATH; ?>/img/mãe_filha_banner.jpg">
+    <div class="retangulo_banner">
+        <h1 title="Sude mental e bem estar">Saúde mental e bem estar!</h1>
+        <p title=" sobre a chamada do site"> 
+            O tabu de se procurar um especialista em saúde mental somente quando se acredita ter algum transtorno grave,
+            deve ser quebrado. Sendo assim, buscar manter o equilíbrio entre saúde física, mental e emocional, tem sido cada
+            vez mais comum. 
+        </p>
+    </div>
+</section>
+<section title="Terapias" id="terapias">
+    <div title="terapias" class="titulo_terapias">
+        <div class="organizar_div2">
+            <img title="terapias" alt="logo" src="<?= INCLUDE_PATH; ?>/img/logo2.png" width="40">
+            <h1 title="Terapias" class="">Terapias</h1>
+        </div>
+    </div>
+    <div class="backgroud">
+        <div class="cd-tabs">
+            <nav>
+                <ul class="cd-tabs-navigation">
+                    <li><a data-content="inbox" class="selected" href="#0"><h2>Terapia Comportamental o que é?</h2></a></li>
+                    <li><a data-content="new" href="#0"><h2>Como a Psicologia Comportamental atua?</h2></a></li>
+                    <li><a data-content="gallery" href="#0"><h2>Quando é indicada?</h2></a></li>                        
+                </ul> <!-- cd-tabs-navigation -->
+            </nav>
+
+            <ul class="cd-tabs-content">
+                <li data-content="inbox" class="selected">
+                    <p>A Terapia Comportamental baseia-se nas propostas do Behaviorismo (termo que tem origem na palavra inglesa behavior, que significa comportamento). Por esta concepção, os comportamentos disfuncionais que fazem as pessoas sofrerem são aprendidos, e neste sentido a terapia é o espaço para a aprendizagem de novos comportamentos mais adaptativos e livres de sofrimento, considerando que a pessoa tem uma historia de vida e está inserida em  seu contexto social.</p>
+                </li>
+                <li data-content="new">
+                    <p>Inicialmente é preciso identificar no indivíduo quais são os comportamentos limitantes que causam dor, sofrimento, prejuízos emocionais e sociais. Por meio de técnicas específicas, o terapeuta analisa a relação do indivíduo com o ambiente, entende quando e onde esses comportamentos ocorrem e quais funções eles têm para a pessoa: se proteger, ser reconhecido, chamar a atenção, ser amado — entre centenas de estratégias que o indivíduo adota para atender suas necessidades e desejos</p>
+
+                    <p>O psicólogo comportamental se apoia em um campo da Psicologia denominado Análise do Comportamento - estudo do comportamento humano de maneira cientifica. Não segue crenças, dogmas ou o senso comum, a Análise do Comportamento faz pesquisas criteriosas e conta com pesquisadores academicamente conceituados em diversas Universidades ao redor do mundo inteiro.</p>
+                </li>
+                <li data-content="gallery">
+                    <p>Quando há manifestação de sofrimento ou dificuldades. A atuação do psicólogo comportamental se faz muito eficiente em quadros de transtornos de ansiedade; compulsões; pânico; fobias; depressão; stress; doenças psicossomáticas; transtorno bipolar; transtornos sexuais; coadjuvante em transtornos de aprendizagem etc. O psicólogo comportamental será sempre uma importante fonte de auxílio, sempre que não nos percebemos capazes de lidar sozinhos com questões impostas pelas mais complicadas situações do dia a dia pois trabalha no desenvolvimento de novos repertórios e ensina a identificar o que determinou o aparecimento de tais comportamentos disfuncionais e o quê os mantêm.</p>
+                </li>                   
+            </ul> <!-- cd-tabs-content -->
+        </div> <!-- cd-tabs -->
+    </div>
+
+</section>
+<script src="_cdn/main.js"></script> <!-- Resource jQuery -->
+<section title="Publico atendido" id="publico_atendido">
+    <div title="público atendido" class="titulo_publico">
+        <div class="organizar_div3">
+            <img title="" alt="logo" src="<?= INCLUDE_PATH; ?>/img/logo2.png" width="40">
+            <h1 title="publico" class="">Público atendido</h1>
+        </div>
+    </div>
+    <h2>O terapeuta comportamental atende o público:</h2>
+    <div class="publico">
+        <div class="infantil">
+            <img alt="crianças" title="infantil" src="<?= INCLUDE_PATH; ?>/img/infantil.jpg">
+            <h3>Infantil</h3>
+        </div>
+        <div class="adolescentes">
+            <img alt="adolescentes" title="adolescentes" src="<?= INCLUDE_PATH; ?>/img/adolescentes.JPG">
+            <h3>Adolescente</h3>
+        </div>
+        <div class="adultos">
+            <img alt="Adultos" title="adultos" src="<?= INCLUDE_PATH; ?>/img/adultos .jpg">
+            <h3>Adulto</h3>
+        </div>
+    </div>
+</section>
+<section title="Sobre a Mayra Silva" id="sobre_mim">
+    <div title="sobre mim" class="titulo_sobre">
+        <div class="organizar_div">
+            <img title="" alt="logo" src="<?= INCLUDE_PATH; ?>/img/logo2.png" width="40">
+            <h1 title="sobre mim" class="">Sobre Mayra Silva</h1>
+        </div>
+    </div>
+    <div title="um pouco mais sobre Mayra silva" class="sobre_mayra">
+        <figure class="foto_maira">
+            <img src="<?= INCLUDE_PATH; ?>/img/foto da maira.jpg" class="maira_img"alt="foto da maira"/>
+            <figcaption class="discrição_foto_maira">
+                Mayra Silva psicóloga,
+                CRP 16/3420.
+            </figcaption>
+        </figure>                
+        <article class="texto_mayra" title="Mayra Silva psicóloga">
+            <h2 class="esconder">Mayra Silva psicóloga</h2>
+
+            <p class="p_1">
+                <strong class="marcar">Mayra Silva</strong> psicóloga, pós Graduada em Gestão de Pessoas, possui cursos de extensão nas área
+                de Psicologia Comportamental, Habilidades Sociais e Assertividade.
+            </p>
+
+            <p class="p_2">
+                Experiência profissional como Terapeuta Comportamental, Analista de Recursos Humanos;
+                Saúde Ocupacional; Psicóloga na rede Socioassistencial, Vasta experiência no treinamento
+                e desenvolvimento de pessoas e liderança de Equipes.
+            </p>
+        </article>
+    </div>
+</section>
+<section title="consultas" id="consultas">
+    <div title="público atendido" class="titulo_consultas">
+        <div class="organizar_div4">
+            <img title="" alt="logo" src="<?= INCLUDE_PATH; ?>/img/logo2.png" width="40">
+            <h1 title="consultas" class="">Consultas</h1>
+        </div>
+    </div>      
+    <div>              
+        <br />
+        <div class="inner contact"> 
+            <div class="inf_consu">
+                <p>Para obter maiores  informações  sobre  os  serviços  prestados, ou agendamento  de 
+                    consultas presenciais. Entre em contato com  este profissional  através  do  site ou pelos telefones abaixo!</p>
+            </div>
+            <!-- Form Area -->
+            <div class="contact-form">                        
+                <!-- Form -->
+                <form id="contact-us" method="post" action="contato.php">
+                    <!--   Left Inputs -->
+                    <div class="col-xs-12 col-sm-6 wow animated slideInLeft" data-wow-delay=".5s">
+                        <!-- Name -->
+                        <input type="text" name="name" id="name" required="required" class="form" placeholder="Nome" />
+                        <!-- Email -->
+                        <input type="email" name="mail" id="mail" required="required" class="form" placeholder="E-mail" />
+                        <!-- Subject -->
+                        <input type="text" name="phone" id="phone" required="required" class="form" placeholder="Telefone" />
+                    </div><!-- End Left Inputs -->
+                    <!-- Right Inputs -->
+                    <div class="col-xs-12 col-sm-6 wow animated slideInRight" data-wow-delay=".5s">
+                        <!-- Message -->
+                        <textarea name="message" id="message" class="form textarea"  placeholder="Menssagem"></textarea>
+                    </div><!-- End Right Inputs -->
+                    <!-- Bottom Submit -->
+                    <div class="relative fullwidth col-xs-12">
+                        <!-- Send Button -->
+                        <button type="submit" id="submit" name="submit" class="form-btn semibold">Enviar Menssagem</button> 
+                    </div><!-- End Bottom Submit -->
+                    <!-- Clear -->
+                    <div class="clear"></div>
+                </form>
+
+                <!-- Your Mail Message -->
+                <div class="mail-message-area">
+                    <!-- Message -->
+                    <div class="alert gray-bg mail-message not-visible-message">
+                        <strong>Thank You !</strong> Your email has been delivered.
+                    </div>
+                </div>
+
+            </div><!-- End Contact Form Area -->
+        </div><!-- End Inner -->
+    </div>
+</section>     
